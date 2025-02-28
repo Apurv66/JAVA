2b9023872ead7962166367ade5db910f9c5d4865
Apurv66 <159132106+Apurv66@users.noreply.github.com>
2025-02-28 11:33:17 +0530

    Add files via upload

---
+++ TypeConversion/AutometicCasting.java
@@ -0,0 +1,9 @@
+public class AutometicCasting {
+    public static void main(String args[]){
+        int a = 8;
+        double b = a;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ TypeConversion/TypeCasting.java
@@ -0,0 +1,9 @@
+public class TypeCasting {
+    public static void main(String args[]){
+        float a = 3.14f;
+        int b = (int) a;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
