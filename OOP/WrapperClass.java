aafdc3f1379571e0d08226679e3523da518a5f54
Apurv66 <159132106+Apurv66@users.noreply.github.com>
2025-03-03 10:19:49 +0530

    Add files via upload

---
+++ OOP/WrapperClass.java
@@ -0,0 +1,21 @@
+class WrapperClass {
+    public static void main(String args[]){
+        Byte a = 10;
+        Short b = 20;
+        Integer c = 40;
+        Long d = 80l;
+        Float e = 4.5f;
+        Double f = 8.22;
+        Character g = 'a';
+        Boolean h = true;
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
