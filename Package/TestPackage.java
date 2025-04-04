3be6299a39607a3b305e924292f26bcfe2ed467c
Apurv66 <159132106+Apurv66@users.noreply.github.com>
2025-04-04 11:26:36 +0530

    Add files via upload

---
+++ Package/TestPackage.java
@@ -0,0 +1,7 @@
+package mypack;
+
+public class TestPackage {
+    public void test() {
+        System.out.println("test method");
+    }
+}
---
+++ Package/UDPackageDemo.java
@@ -0,0 +1,9 @@
+import mypack.TestPackage;
+
+class UDPackageDemo {
+    public static void main(String args[]) {
+        TestPackage tp = new TestPackage();
+        
+        tp.test();
+    }    
+}
